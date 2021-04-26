7990f60835c60c0e03bc909b57462b5764d9bef5
mwcz <mwc@clayto.com>
2021-04-26 16:30:18 -0400

    add token splitting code

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,9 +1,137 @@
-import { greet } from './main'
+import {test, expect, describe} from "@jest/globals";
+import {createTokenizer} from "./main"
 
-test('the data is peanut butter', () => {
-  expect(1).toBe(1)
-});
+describe("the createTokenizer function", () => {
 
-test('greeting', () => {
-  expect(greet('Foo')).toBe('Hello Foo')
+  test("exists", () => {
+    expect(createTokenizer).toBeInstanceOf(Function);
+  });
+
+  describe("tokenize function", () => {
+    test("returns expected values", () => {
+
+      const tokConfig = {
+        tokens: [
+          {name: "test_token", start: "a", end: "c"}
+        ]
+      };
+
+      const tok = createTokenizer(tokConfig);
+      const output = tok.tokenize("abc");
+
+      expect(output).toStrictEqual({
+        input: "abc",
+        config: tokConfig,
+        result: [
+          {
+            name: "test_token",
+            content: "b",
+            startIndex: 1,
+            endIndex: 2,
+            wrappedContent: "abc",
+            wrappedStartIndex: 0,
+            wrappedEndIndex: 3,
+          }
+        ]
+      });
+
+    });
+
+    test("handles multiline strings", () => {
+
+      const tokConfig = {
+        tokens: [
+          {name: "one", start: "<!-- ONE -->", end: "<!-- /ONE -->"},
+          {name: "two", start: "<!-- TWO -->", end: "<!-- /TWO -->"},
+          {name: "three", start: "<!-- THREE -->", end: "<!-- /THREE -->"},
+        ]
+      };
+
+      const tok = createTokenizer(tokConfig);
+      const input = `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <!-- ONE -->
+            one
+            <!-- /ONE -->
+          </head>
+          <body>
+            <!-- TWO -->
+            two
+            <!-- /TWO -->
+            <!-- THREE -->
+            three
+            <!-- /THREE -->
+          </body>
+        </html>
+      `;
+
+      const output = tok.tokenize(input);
+
+      expect(output).toStrictEqual({
+        input,
+        config: tokConfig,
+        result: [
+          {
+            name: "one",
+            content: "\n            one\n            ",
+            startIndex: 81,
+            endIndex: 110,
+            wrappedContent: "<!-- ONE -->\n            one\n            <!-- /ONE -->",
+            wrappedStartIndex: 69,
+            wrappedEndIndex: 123,
+          }, {
+            name: "two",
+            content: "\n            two\n            ",
+            startIndex: 183,
+            endIndex: 212,
+            wrappedContent: "<!-- TWO -->\n            two\n            <!-- /TWO -->",
+            wrappedStartIndex: 171,
+            wrappedEndIndex: 225,
+          }, {
+            name: "three",
+            content: "\n            three\n            ",
+            startIndex: 252,
+            endIndex: 283,
+            wrappedContent: "<!-- THREE -->\n            three\n            <!-- /THREE -->",
+            wrappedStartIndex: 238,
+            wrappedEndIndex: 298,
+          }
+        ]
+      });
+
+    });
+
+    test("accepts regex characters", () => {
+
+      const tokConfig = {
+        tokens: [
+          {name: "regex_tokens", start: /a.b/, end: /d.e/}
+        ]
+      };
+
+      const tok = createTokenizer(tokConfig);
+      const output = tok.tokenize("axbcdxe");
+
+      expect(output).toStrictEqual({
+        input: "axbcdxe",
+        config: tokConfig,
+        result: [
+          {
+            name: "regex_tokens",
+            content: "c",
+            startIndex: 3,
+            endIndex: 4,
+            wrappedContent: "axbcdxe",
+            wrappedStartIndex: 0,
+            wrappedEndIndex: 7,
+          }
+        ]
+      });
+
+    });
+
+  });
+
 });
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,93 @@
-export const delayMillis = (delayMs: number): Promise<void> => new Promise(resolve => setTimeout(resolve, delayMs));
+/**
+ * Configuration options for a single token.
+ */
+export interface TokenConfig {
+  start: string | RegExp,
+  end: string | RegExp,
+  name: string,
+}
 
-export const greet = (name: string): string => `Hello ${name}`
+/**
+ * Configuration options for tokenizers.
+ */
+export interface TokenizerConfig {
+  // configuration for the tokens to find
+  tokens: TokenConfig[],
+}
 
-export const foo = async (): Promise<boolean> => {
-  console.log(greet('World'))
-  await delayMillis(1000)
-  console.log('done')
-  return true
+/**
+ * The result of a tokenization.
+ */
+export interface TokenizerResult {
+  input: string, // the original input string
+  config: TokenizerConfig, // the configuration that was used for this tokenization
+  result: TokenResult[],
+}
+
+export interface TokenResult {
+  startIndex: number,
+  endIndex: number,
+  name: string,
+  config?: TokenizerConfig,
+  content: string,
+  wrappedContent: string,
+  wrappedStartIndex: number,
+  wrappedEndIndex: number,
+}
+
+class Tokenizer {
+  #config: TokenizerConfig;
+
+  constructor(config: TokenizerConfig) {
+    this.#config = config;
+  }
+
+  /**
+   * Tokenize a string.
+   */
+  tokenize(input: string): TokenizerResult {
+
+    const result: TokenResult[] = [];
+
+    for (const token of this.#config.tokens) {
+      const reFlags = ["s"];
+
+      if (token.start instanceof RegExp) {
+        reFlags.push(token.start.flags);
+        token.start = token.start.source;
+      }
+      if (token.end instanceof RegExp) {
+        reFlags.push(token.end.flags);
+        token.end = token.end.source;
+      }
+
+      const reWrap = new RegExp(`(${token.start})(.*?)(${token.end})`, reFlags.join(""));
+
+      const wrap = reWrap.exec(input);
+
+      if (wrap && wrap[0]) {
+        result.push(
+          {
+            startIndex: wrap.index + wrap[1].length,
+            endIndex: wrap.index + wrap[1].length + wrap[2].length,
+            name: token.name,
+            content: wrap[2],
+            wrappedContent: wrap[0],
+            wrappedStartIndex: wrap.index,
+            wrappedEndIndex: wrap.index + wrap[0].length,
+          }
+        );
+      }
+    }
+
+    return {
+      input,
+      result,
+      config: this.#config,
+    };
+  }
+}
+
+export function createTokenizer(config: TokenizerConfig): Tokenizer {
+  return new Tokenizer(config);
 }
